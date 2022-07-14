e568b0f2a07081474300d149e2c05f1571fba53c
amolu <amolujagare@gmail.com>
2022-07-14 09:06:29 +0530

    Initial commit

---
+++ src/DataTypeDemos.java
@@ -0,0 +1,20 @@
+public class DataTypeDemos {
+
+    public static void main(String[] args) {
+
+        byte b = -127;
+
+        short s = 32767;
+
+
+        int a = 10;
+
+        double d = 11.45;
+        float f = 11.34f;
+
+        char c = 'u';
+
+        boolean b1 = true; // primitive
+        boolean b2 = false;
+    }
+}
